b307828a37b824960aa57f7664b2da492f59ef81
dominic <dominicseduraja3@gmail.com>
2024-10-08 23:48:23 +0530

    hallow-square-patern

---
+++ patern/01-Hallow-Square-Pattern.js
@@ -0,0 +1,21 @@
+let number = 5;
+let string = '';
+
+for(let i=1; i<=number; i++){
+  for(let j=1; j<=number; j++){
+    if(i===1 || i===number){
+      string += '*';
+    }else{
+      if(j===1 || j===number){
+        string += '*'
+      }else{
+        string += ' '
+      }
+    }
+  }
+  string += '\n';
+}
+console.log('\n');
+console.log('\n');
+console.log(string);
+
