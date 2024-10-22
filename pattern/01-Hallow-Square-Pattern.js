d8203bcc42171e95fa1bb2bad71bfdbcd12c9cfc
Dominic-SR <dominicseduraja3@gmail.com>
2024-10-22 23:48:17 +0530

    neww added

--- pattern/01-Hallow-Square-Pattern.js
+++ pattern/01-Hallow-Square-Pattern.js
@@ -3,18 +3,18 @@ let string = '';
 
 for(let i=1; i<=number; i++){
   for(let j=1; j<=number; j++){
-  
-   if(i===1 || i===number){
-      string += '*';
+ 
+  if(i===1 || i===number){
+      string += "*";
    }else{
       if(j===1 || j===number){
-        string += '*'
+        string += "*";
       }else{
         string += ' '
       }
     }
-   
-  }
+  
+ }
    string += '\n';
 }
 
