43181721131eb8825df0209969b8a8cdb4d8f93e
Dominic-SR <dominicseduraja3@gmail.com>
2024-11-27 23:47:18 +0530

    remove duplicat array using filter and IndexOf

--- JS-Interview-Questions/05-Remove-Duplicates-Array.js
+++ JS-Interview-Questions/05-Remove-Duplicates-Array.js
@@ -1,5 +1,9 @@
-const removeDuplicates =(arr) =>{
-	return [...new Set(arr)]
+// const removeDuplicates =(arr) =>{
+//	return [...new Set(arr)]
+// }
+
+const removeDuplicates =(arr)=>{
+ return arr.filter((item,index,self)=> self.indexOf(item) == index )
 }
 
 let arr = [1,2,2,3,4,4,5]
