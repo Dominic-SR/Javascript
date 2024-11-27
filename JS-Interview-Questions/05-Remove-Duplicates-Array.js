f5e6f9de380ce2794afb2d0066a8f43c3262731e
Dominic-SR <dominicseduraja3@gmail.com>
2024-11-27 23:58:54 +0530

    remove duplicat array using reduce

--- JS-Interview-Questions/05-Remove-Duplicates-Array.js
+++ JS-Interview-Questions/05-Remove-Duplicates-Array.js
@@ -2,8 +2,16 @@
 //	return [...new Set(arr)]
 // }
 
-const removeDuplicates =(arr)=>{
- return arr.filter((item,index,self)=> self.indexOf(item) == index )
+// const removeDuplicates =(arr)=>{
+// return arr.filter((item,index,self)=> self.indexOf(item) == index )
+//}
+
+const removeDuplocates = (arr) => {
+
+arr.reduce((unique,item) => {
+return unique.includes(item) ? unique : [...unique, item ]
+},[])
+
 }
 
 let arr = [1,2,2,3,4,4,5]
