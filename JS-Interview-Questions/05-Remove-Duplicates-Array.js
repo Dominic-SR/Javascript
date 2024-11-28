3762b7690b87a4973ad6b5da9c53f72e2ff50928
Dominic-SR <dominicseduraja3@gmail.com>
2024-11-28 23:24:13 +0530

    duplicate array remove issues solved

--- JS-Interview-Questions/05-Remove-Duplicates-Array.js
+++ JS-Interview-Questions/05-Remove-Duplicates-Array.js
@@ -1,18 +1,22 @@
+// using Set()
 // const removeDuplicates =(arr) =>{
 //	return [...new Set(arr)]
 // }
 
+// using filter and indexOf
 // const removeDuplicates =(arr)=>{
 // return arr.filter((item,index,self)=> self.indexOf(item) == index )
 //}
 
-const removeDuplocates = (arr) => {
+// using reduce and included
+// const removeDuplicates =(arr)=>{
+// return arr.reduce((unique,item) => // {
+// return unique.includes(item) ? uniq// ue : [...unique, item ]},[])
+// }
 
-arr.reduce((unique,item) => {
-return unique.includes(item) ? unique : [...unique, item ]
-},[])
+// using for and includes
 
-}
 
 let arr = [1,2,2,3,4,4,5]
 console.log("--->",removeDuplicates(arr))
+
