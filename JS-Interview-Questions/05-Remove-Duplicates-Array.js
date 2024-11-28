90f2ce735bd41b24eb5e1a5ab271676564f50f99
Dominic-SR <dominicseduraja3@gmail.com>
2024-11-28 23:43:30 +0530

    remove array using for loop and includes' ' git push

--- JS-Interview-Questions/05-Remove-Duplicates-Array.js
+++ JS-Interview-Questions/05-Remove-Duplicates-Array.js
@@ -16,6 +16,18 @@
 
 // using for and includes
 
+const removeDuplicates = (arr) => {
+
+const getArr = []
+console.log("???",arr.length)
+ for(let i=0; arr.length - 1 >= i; i++){
+
+	if(!getArr.includes(arr[i])){
+	   getArr.push(arr[i]);
+	}
+  }
+return getArr;
+}
 
 let arr = [1,2,2,3,4,4,5]
 console.log("--->",removeDuplicates(arr))
