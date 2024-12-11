47ed89e747a776616c303db8f3013e73d6999074
Dominic-SR <dominicseduraja3@gmail.com>
2024-12-11 23:51:14 +0530

    string balanved updated'

--- JS-Interview-Questions/06-String-Balanced-Parentheses.js
+++ JS-Interview-Questions/06-String-Balanced-Parentheses.js
@@ -3,11 +3,12 @@ const balanced = (str) =>{
  foreach(const char of str){
    if(char == '('){
 	stack.push(char);
-   }else{
+   }else if(char == ')'){
 	if(stack.length == 0){
 	return false
 	}
 	stack.pop()
-   }
+   }else{
+	console.log("")
  }
 }
