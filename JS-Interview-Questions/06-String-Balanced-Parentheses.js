e00d1e248d916d0b3de7c6894a328756be503c6e
Dominic-K2B <k2b.dominic@gmail.com>
2024-12-13 23:49:28 +0530

    balanced completed

--- JS-Interview-Questions/06-String-Balanced-Parentheses.js
+++ JS-Interview-Questions/06-String-Balanced-Parentheses.js
@@ -1,14 +1,16 @@
 const balanced = (str) =>{
  const stack = [];
- foreach(const char of str){
-   if(char == '('){
-	stack.push(char);
-   }else if(char == ')'){
-	if(stack.length == 0){
-	return false
-	}
-	stack.pop()
-   }else{
-	console.log("")
- }
+ 	for(const char of str){
+   		if(char == '('){
+			stack.push(char);
+   		}else if(char == ')'){
+			if(stack.length == 0){
+	 		return false
+		}
+			stack.pop()
+   		}
 }
+return stack.length === 0;
+}
+
+console.log(balanced("())"));
