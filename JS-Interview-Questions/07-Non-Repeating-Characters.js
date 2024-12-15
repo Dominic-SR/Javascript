0a909f7fd387e1c64226fa88e75d1a92c0fca2fc
Dominic-SR <dominicseduraja3@gmail.com>
2024-12-15 22:00:34 +0530

    non repeating characters done

--- JS-Interview-Questions/07-Non-Repeating-Characters.js
+++ JS-Interview-Questions/07-Non-Repeating-Characters.js
@@ -1,5 +1,19 @@
-const firstNonRepeatingCharacters=()=>{
-    for(let){
-	console.log("")
+const firstNonRepeatingCharacters=(str)=>{
+    const repeatingChar = []
+      for(const char of str){
+	if(repeatingChar[char]){
+	  repeatingChar[char]+=1;
+	}else{
+	  repeatingChar[char]=1;
 	}
+      }
+
+      for(const char of str){
+	if(repeatingChar[char] === 1){
+     return char;
+        }
+	}
+return null
 }
+let result = firstNonRepeatingCharacters("ssssetttt")
+console.log(result);
