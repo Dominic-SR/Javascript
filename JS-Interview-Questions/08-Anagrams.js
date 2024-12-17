932235884a3fb27fcb88449e0574ef4a56aa5245
Dominic-SR <dominicseduraja3@gmail.com>
2024-12-17 23:11:30 +0530

    08 anagrams length check done

--- JS-Interview-Questions/08-Anagrams.js
+++ JS-Interview-Questions/08-Anagrams.js
@@ -1,5 +1,10 @@
 const areAnagrams = (str1,str2) ={
  str1 = str1.replace('/\s+/g','').toLowerCase();
  str2 = str2.replace("/\s+/g",'').toLowerCase();
+
+ if(str1.lemgth != str2.length){
+	return false;
+ }
+
 }
 let result = areAnagrams();
