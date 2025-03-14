ad80756e27c359da5cec84e47cbd0eb4e36677a4
Dominic-SR <dominicseduraja3@gmail.com>
2025-03-14 23:57:21 +0530

    update element and length

--- JS-DSA/1.Array.js
+++ JS-DSA/1.Array.js
@@ -1,5 +1,12 @@
 const fruits = ["apple", "banana", "cherry"];
 
 //Accessing elements
-console.log(fruits[0]); //apple
-console.log(fruits[1]); //bannana
+console.log(fruits[0]); // apple
+console.log(fruits[1]); // bannana
+
+//Update elements
+fruits[2] ="blueberry";
+console.log(fruits); // [ 'apple', 'banana', 'blueberry' ]
+
+// Find the total number of elements
+console.log(fruits.length); // 3
