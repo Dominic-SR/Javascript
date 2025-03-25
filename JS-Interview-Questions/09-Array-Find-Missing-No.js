4f2d2ce794c149c9898289363cbcfd85e6b38d09
Dominic-SR <dominicseduraja3@gmail.com>
2025-03-25 23:57:22 +0530

    sum of number

--- JS-Interview-Questions/09-Array-Find-Missing-No.js
+++ JS-Interview-Questions/09-Array-Find-Missing-No.js
@@ -1,1 +1,11 @@
+// Example 1:
+
+const findMissingNoInArray=(arr)=>{
+ const n = arr.length + 1;
+
+ //Sum of number from 1 to N
+ const totalSum = (n * (n+1)/2)
+
+ //Sum of num in array
+}
 console.log("hello")
