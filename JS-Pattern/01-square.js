78005fd8192d08c4ef1cfc1f8d318f5769bef238
Dominic-SR <dominicseduraja3@gmail.com>
2025-09-23 23:47:31 +0530

    square pattern1

--- JS-Pattern/01-square.js
+++ JS-Pattern/01-square.js
@@ -1,4 +1,17 @@
-let n=5;
-for(i=0; i<n; i++){
-    console.log("*".repeat(n));
+// method-1
+// let n=5;
+// for(i=0; i<n; i++){
+//     console.log("*".repeat(n));
+// }
+
+//method-2
+let num=5;
+
+for(i=1;i<=num;i++){
+    let data=""
+    for(j=1;j<=i;j++){
+        data+= "*"
+    }
+    console.log("--->",data);    
 }
+
